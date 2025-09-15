d70a5260f7dd44f7e5cbd4867965ca635762de36
nominik <mike.nogai.spb@mail.ru>
2025-09-16 00:43:53 +0300

    change1

--- src/Basket.java
+++ src/Basket.java
@@ -1,10 +1,25 @@
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Basket {
-    ArrayList<Goods> goods = new ArrayList<>();
+    public Map<Goods, Integer> myBasket = new TreeMap<>();
+    public double sum = 0;
 
+    public void addGood(Goods good, Integer quantity) {
+
+    }
+
+    public void delGood(Goods good) {
+
+    }
+
+    public void findDelay() {
+
+    }
 
     public void clear() {
-        goods.clear();
+        myBasket.clear();
+        sum = 0;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -11,19 +11,20 @@ public class Main {
         //создаем продуктовые товары
         ArrayList goods = new ArrayList<Goods>();
         goods.add(new foodGoods("154786", "Колбаса докторская. 400гр", 325.56, setDate("2025-11-15")));
-        goods.add(new foodGoods ("154754","Сыр Российский. 180гр",220.00, setDate("2025-09-01")));
-        goods.add(new foodGoods ("154786","Масло сливочное. 180гр",255.99, setDate("2026-01-12")));
+        goods.add(new foodGoods("154754", "Сыр Российский. 180гр", 220.00, setDate("2025-09-01")));
+        goods.add(new foodGoods("154786", "Масло сливочное. 180гр", 255.99, setDate("2026-01-12")));
         //создаем не продуктовые товары
-        goods.add(new houseGoods ("129562","Шампунь-кондиционер. 450мл",412.20));
-        goods.add(new houseGoods ("124002","Туалетная бумага. 4рул",160.20));
+        goods.add(new houseGoods("129562", "Шампунь-кондиционер. 450мл", 412.20));
+        goods.add(new houseGoods("124002", "Туалетная бумага. 4рул", 160.20));
 
         //создаем экземпляр корзины
         Basket myBasket = new Basket();
+        //myBasket.sum = 200;
 
-        //выводим на экран все доступные к заказу товары
+        //выводим на экран товары в магазине
         printGoods(goods);
 
-        //выводим на экран все доступные действия
+        //выводим на экран доступные действия
         printMenu(myBasket);
 
 //        Scanner scanner = new Scanner(System.in);
@@ -39,39 +40,50 @@ public class Main {
     }
 
     private static void printMenu(Basket myBasket) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите номер следующего действия");
-        System.out.println("1. Добавить в корзину");
-        System.out.println("2. Удалить из корзины");
-        System.out.println("3. Найти просрочку в корзине (только прод.товары)");
-        System.out.println("4. Оценить товар в корзине");
-        System.out.println("5. Оплатить товар в корзине");
-        System.out.print(  "   Введите номер действия:");
-        int action = scanner.nextInt();
-        switch (action) {
-            case 1:
-                addToBasket();
-                break;
-            case 2:
-                delFromBasket();
-                break;
-            case 3:
-                findDelay();
-                break;
-            case 4:
-                addGradle();
-                break;
-            case 5:
-                payBasket(myBasket);
-                break;
-            default:
-                System.out.println("некорректный ввод...");
-                printMenu(myBasket);
+
+        if (myBasket.sum > 0) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("----------- Действия с корзиной");
+            System.out.println("1. Добавить в корзину");
+            System.out.println("2. Удалить из корзины");
+            System.out.println("3. Найти просрочку в корзине (только прод.товары)");
+            System.out.println("4. Оценить товар в корзине");
+            System.out.println("5. Оплатить товар в корзине");
+            System.out.print("   Введите номер действия: => ");
+
+            int action = scanner.nextInt();
+            switch (action) {
+                case 1:
+                    addToBasket();
+                    break;
+                case 2:
+                    delFromBasket();
+                    break;
+                case 3:
+                    findDelay();
+                    break;
+                case 4:
+                    addGradle();
+                    break;
+                case 5:
+                    payBasket(myBasket);
+                    break;
+                default:
+                    System.out.println("---------- некорректный ввод...");
+                    printMenu(myBasket);
+            }
+        } else {
+            System.out.println("---------- Корзина пуста...");
+            addToBasket();
         }
+
+
     }
 
     private static void payBasket(Basket myBasket) {
         myBasket.clear();
+        System.out.println("---------- Оплачено...");
+        System.out.println("---------- Корзина очищена...");
     }
 
     private static void addGradle() {
@@ -84,23 +96,34 @@ public class Main {
     }
 
     private static void addToBasket() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---------- Добавление товаров в корзину...");
+        String action = scanner.nextLine();
+        System.out.print("Введите номер продукта (N) и количество (Q) в формате 'N-Q': => ");
+
     }
 
-    public static void printGoods(ArrayList goods){
+    public static void printGoods(ArrayList goods) {
+        System.out.println("---------- Товары в нашем магазине...");
         for (int i = 0; i < goods.size(); i++) {
             Goods str = (Goods) goods.get(i);
-            StringBuilder result = new StringBuilder();
-            result.append(i+1)
-                    .append(" - ")
-                    .append(str.getArticle())
-                    .append(" / ")
-                    .append(str.getName())
-                    .append(" / ")
-                    .append(str.getPrice())
-                    .append("руб. / рейтинг ")
-                    .append(str.getRating());
-            System.out.println(result);
+            printGood(str, i);
         }
+    }
+
+    public static void printGood(Goods str, int i) {
+        StringBuilder result = new StringBuilder();
+        result.append(i + 1)
+                .append(" - ")
+                .append(str.getArticle())
+                .append(" / ")
+                .append(str.getName())
+                .append(" / ")
+                .append(str.getPrice())
+                .append("руб. / рейтинг ")
+                .append(str.getRating());
+        System.out.println(result);
     }
 
     public static Date setDate(String expDate) {
